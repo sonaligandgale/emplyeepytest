c7842a882eb481ac1aaaf42bc7387c4755724f56
sonaligandgale <sonaligandgalekar@gmail.com>
2025-12-15 23:35:58 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,1 +1,19 @@
+import pytest
 
+def employee_details(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Employee Department: {department}\n"
+        f"Employee Salary: {salary}\n"
+    )
+    return result
+
+
+if __name__ == "__main__":
+    name = "Alice"
+    emp_id = "E1001"
+    department = "IT"
+    salary = 55000
+
+    print(employee_details(name, emp_id, department, salary))
